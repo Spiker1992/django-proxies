bc3ca25d4a171cfbcca3c144c87631ad9844a230
Vladislav Ogir <vlad@ogir.co.uk>
2024-02-29 22:19:34 +0000

    feat: updated use of signal receivers to make AppConfig leaner

--- delivery_progress/apps.py
+++ delivery_progress/apps.py
@@ -5,7 +5,4 @@ class DeliveryProgressConfig(AppConfig):
     name = 'delivery_progress'
 
     def ready(self):
-        from . import signals, listeners
-
-        signals.order_was_paid.connect(listeners.mark_order_as_paid)
-        signals.order_was_picked_up.connect(listeners.mark_order_as_picked_up)
+        from . import listeners
--- delivery_progress/listeners.py
+++ delivery_progress/listeners.py
@@ -1,8 +1,10 @@
 from django.dispatch import receiver
 
+from . import signals
 from delivery_progress.models import STATUS_ORDER_PAID, STATUS_ORDER_PICKED_UP, Delivery
 
 
+@receiver(signals.order_was_paid)
 def mark_order_as_paid(sender, order_id, **kwargs):
     delivery: Delivery = (
         Delivery.objects
@@ -13,6 +15,7 @@ def mark_order_as_paid(sender, order_id, **kwargs):
     delivery.status = STATUS_ORDER_PAID
     delivery.save()
 
+@receiver(signals.order_was_picked_up)
 def mark_order_as_picked_up(sender, order_id, **kwargs):
     delivery: Delivery = (
         Delivery.objects
