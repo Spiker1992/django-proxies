b02869d5a8ab4a32ab5df76b44e0a09e93e71528
Vladislav Ogir <v.v.ogir@outlook.com>
2024-04-03 23:50:54 +0100

    added an aggregate class to use for other aggregates

--- event_sourcing/aggregates.py
+++ event_sourcing/aggregates.py
@@ -1,43 +1,54 @@
 from dataclasses import asdict
+import uuid
 from event_sourcing.dataclasses import PostCreated
 from event_sourcing.models import CreatedPostEvent
+from django.db import models
 from event_sourcing.models.post_events import PostEvents, PostUpdatedEvent, ReadyForReviewEvent
 
-
-class PostAggregate():
+class Aggregate():
     events = []
+    event_store: models.Model
+    mapper  = {}
+
+    def __init__(self) -> None:
+        self.events = []
+
+    def _apply_event(self, event):
+        apply_method_name = self.mapper.get(event.__class__)
+        if apply_method_name:
+            self.__getattribute__(apply_method_name)(event)
+
+    def _persist_event(self, event):
+        event.save()
+
+    def load(self, stream_id: uuid.UUID):
+        events = list(self.event_store.objects.filter(stream_id=stream_id).all())
+
+        for event in events:
+            self._apply_event(event)
+
+class PostAggregate(Aggregate):
+    # created a mapper for replies
     mapper  = {
        CreatedPostEvent: "_apply_created_post_event",
        PostUpdatedEvent: "_apply_updated_post_event",
        ReadyForReviewEvent: "_apply_ready_for_review_event",
     }
+    event_store = PostEvents
+
     title = None 
     content = None 
     ready_for_review = False
 
-    def __init__(self) -> None:
-        self.events = []
-
-    def load(self, stream_id):
-        events = list(PostEvents.objects.filter(stream_id=stream_id).all())
-
-        for event in events:
-            self.__apply_event(event)
-
-    def __apply_event(self, event):
-        apply_method_name = self.mapper.get(event.__class__)
-        if apply_method_name:
-            self.__getattribute__(apply_method_name)(event)
-
-    def __persist_event(self, event):
-        event.save()
-
-    def create_post(self, stream_id: int, payload: PostCreated):
+    def create_post(self, payload: PostCreated) -> uuid.UUID: 
+        stream_id: uuid.UUID  = uuid.uuid4()
+    
         event = CreatedPostEvent(stream_id=stream_id, event_payload=asdict(payload))
 
-        self.__apply_event(event)
-        self.__persist_event(event)
+        self._apply_event(event)
+        self._persist_event(event)
 
+        return stream_id
 
     def _apply_created_post_event(self, event):
         self.title = event.event_payload.get("title")
